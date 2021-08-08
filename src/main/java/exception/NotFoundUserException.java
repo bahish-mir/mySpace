4d4a6fd8a63d3a8bad0ae5678bd90df97bbd0d69
bahish.mir <mirzemetovb@gmail.com>
2021-08-09 00:31:31 +0300

    added NotFoundUserException.java

---
+++ src/main/java/exception/NotFoundUserException.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class NotFoundUserException extends Exception{
+    private String err;
+
+    public NotFoundUserException() {
+    }
+
+    public NotFoundUserException(String message, String err) {
+        super(message);
+        this.err = err;
+    }
+
+    public String getErr() {
+        return err;
+    }
+}
